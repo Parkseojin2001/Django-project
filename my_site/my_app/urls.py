8ae7cc0adf9cb6737b3cd6f07e8a8bb80efb3850
Parkseojin2001 <seojin0510612@gmail.com>
2024-11-19 21:10:43 +0900

    Dynamic views

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # domaion.com/my_app/simple_view
 urlpatterns = [
-    path('', views.simple_view) #/my_apps --> PROJECT urls.py
+    path('<str:topic>/', views.news_view),
+    path('<int:num1>/<int:num2>/', views.add_view)
 ]
--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def simple_view(request):
-    return HttpResponse("SIMPLE VIEW") # LATER TEMPLATE HTML (JINJA)
+
+articles = {
+    'sports': 'Sports Page',
+    'finance': 'Finance Page',
+    'politics': 'Politics Page'    
+}
+
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    # domain.com/my_app/num1/num2 --> num1+num2
+    add_result = num1 + num2
+    result = f"{num1} + {num2} = {add_result}"
+    return HttpResponse(str(result))
