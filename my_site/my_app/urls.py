c4336f2b20928b5d23848d4eb979882821977e50
Parkseojin2001 <seojin0510612@gmail.com>
2025-04-09 19:22:37 +0900

    Updated

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-# domaion.com/my_app/simple_view
 urlpatterns = [
-    path('<int:num_page>', views.num_page_view),
-    path('<str:topic>/', views.news_view, name='topic-page'),
+    path('', views.simple_view) # domain.com/my_app/simple_view
   
 ]
--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -3,25 +3,7 @@ from django.http import HttpResponse,HttpResponseNotFound, Http404, HttpResponse
 from django.urls import reverse
 # Create your views here.
 
-articles = {
-    'sports': 'Sports Page',
-    'finance': 'Finance Page',
-    'politics': 'Politics Page'    
-}
-
-def news_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        return Http404('404 GENERIC ERROR') # 404.html
-
-# domain.com/my_app/0 ---> domain.com/my_app/sports
-
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys()) # ['sports', 'finance', 'politics']
-    topic = topics_list[num_page]
-    
-    return HttpResponseRedirect(reverse('topic-page', args=[topic]))
+def simple_view(request):
+    return render(request, 'my_app/example.html')  #.html
     
     
